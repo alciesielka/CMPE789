108edc8ec5038e02a134c7cc2557b37e549d725f
Tianna Seitz <tjs1980@rit.edu>
2024-09-10 17:30:11 -0400

    flatten

--- project_1/mytrainer_vgg.py
+++ project_1/mytrainer_vgg.py
@@ -79,7 +79,7 @@ class PowerModeAutopilot(nn.Module):
         # x = self.conv423_5123(x)
         # x = nn.functional.relu(x)        
         x_shape = x.size
-        x = x.flatten(7*7*512) # may need to flatten instead of view ??
+        x = x.flatten(8*8*512) # may need to flatten instead of view ??
         x = self.fc1(x)
         x = self.fc2(x)
         x = nn.functional.softmax(x)
