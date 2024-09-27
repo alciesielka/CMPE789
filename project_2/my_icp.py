a501dbfe067088bfef5be41c4903dc8b59601474
Tianna Seitz <tjs1980@rit.edu>
2024-09-26 22:16:38 -0400

    add icp algorithm

--- project_2/my_icp.py
+++ project_2/my_icp.py
@@ -22,12 +22,16 @@ def estimate_normals(points, k_neighbors=30): # ready to test - TJS
     normals_estimate = o3d.geometry.estimate_normals(search_param=o3d.geometry.KDTreeSearchParamKNN(k_neighbors))
     return normals_estimate
 
-def normal_shooting(source_points, source_normals, target_points, target_normals):
-    pass
-
+def normal_shooting(source_points, source_normals, target_points, target_normals): # ready to test but I think this equation is wrong - TJS
+    # similar to point to plane, find normal and then intersection between another point cloud and based on the intersection we'll find closest point to intersection.
+    # calculate intersection and then find closest point
+    # normal is vertical vector orientation (use estimate_normals)
+    matched_target_points = min(np.sum(((source_points - target_points)*target_normals*source_normals)**2))
+    return matched_target_points
+    
 def point_to_plane(source_points, target_points, target_normals): # ready to test (ensure equation!) - TJS
-    p2pl_error = min(np.sum(((source_points - target_points)*target_normals)**2))
-    return p2pl_error
+    matched_target_points = min(np.sum(((source_points - target_points)*target_normals)**2))
+    return matched_target_points
 
 def compute_transformation(source_points, target_points): # ready to test - TJS (I keep seeing .T be used with np.dot, investigate)
     # Compute the optimal rotation matrix R and translation vector t that align source_points with matched_target_points
@@ -50,7 +54,6 @@ def apply_transformation(source_points, R, t): # Ready to test - TJS
 def compute_mean_distance(source, target): # ready to test - TJS
     # Compute the mean Euclidean distance between the source and the target
     mean_distance = np.mean(((target[0]-source[0])**2)+((target[1]-source[1])**2)+((target[2]-source[2])**2)**0.5)
-    # mean_distance = np.mean(np.linalg.norm(source_points-target_points))
     return mean_distance
 
 def calculate_mse(source_points, target_points): # ready to test, ensure source points is nx3 array - TJS
@@ -70,23 +73,40 @@ def icp(source_points, target_points, max_iterations=100, tolerance=1e-6, R_init
         source_points = apply_transformation(source_points, R_init, t_init)
         
     # ICP algorithm, return the rotation R and translation t
+    # all losses should be about similar
     for i in range(max_iterations):
         # The number of points may be different in the source and the target, so align them first
         if strategy == 'closest_point':
             indices = find_closest_points(source_points, target_points)
             matched_target_points = target_points[indices]
             pass
+
         elif strategy == 'normal_shooting':
+            source_normals = estimate_normals(source_points)
+            target_normals = estimate_normals(target_points)
+            # fix what comes out
+            matched_target_points = normal_shooting(source_points, target_points, source_normals, target_normals)
             pass
+
         elif strategy == "point-to-plane":
+            target_normals = estimate_normals(target_points)
+            matched_target_points= point_to_plane(source_points, target_points, target_normals)
             pass
+
         else:
             raise ValueError("Invalid strategy. Choose 'closest_point', 'normal_shooting', or 'point_to_plane'")
         
         # Complete the rest of code using source_points and matched_target_points
-        pass
+        R,t = compute_transformation(source_points, matched_target_points)
+        aligned_source_points = apply_transformation(source_points, R, t)
+        mean_dist = compute_mean_distance(aligned_source_points, matched_target_points)
+
+        if mean_dist < tolerance:
+            print(f"ICP converged gracefully at {i+1} iterations")
+            return R, t, aligned_source_points
     
-    aligned_source_points = source_points
+        aligned_source_points = source_points
+    print(f"ICP did not converge gracefully after {max_iterations} iterations")
     return R, t, aligned_source_points
 
 if __name__ == "__main__":
@@ -98,7 +118,8 @@ if __name__ == "__main__":
     source_points = load_ply(source_file)
     target_points = load_ply(target_file)
     
-    # Initial guess (modifiable)
+    # Initial guess (modifiable) # good initial guess is important, feel free to go higher
+    # if we do our own data, we can get our initial guess from the ground truth on that and input it here for the original tests!
     R_init = np.array([[1, 0, 0], [0, 1, 0], [0, 0, 1]])
     t_init = np.array([0, 0, 0])
     
