b02adf1d2889120d9a8b50cbba3d06a66ad10206
alciesielka <alexandraciesielka@gmail.com>
2024-10-28 11:05:32 -0400

    added some comments and skeleton code to my_tracker : '

--- project_3/my_tracker.py
+++ project_3/my_tracker.py
@@ -17,7 +17,7 @@ class Siamese_Network(nn.Module):
         self.conv3 = nn.Conv2d(128, 128, kernel_size=3)
         self.fc1 = nn.Linear(128 * 22 * 22, 256)
         self.fc2 = nn.Linear(256, 256)
-def forward_one(self, x):
+    def forward_one(self, x):
         x = F.relu(F.max_pool2d(self.conv1(x), 2))
         x = F.relu(F.max_pool2d(self.conv2(x), 2))
         x = F.relu(self.conv3(x))
@@ -26,33 +26,54 @@ def forward_one(self, x):
         x = self.fc2(x)
         return x
    
-def forward(self, input1, input2):
+    def forward(self, input1, input2):
         output1 = self.forward_one(input1)
         output2 = self.forward_one(input2)
         return output1, output2
 
+    def similarity(self, output1, output2):
+        return F.cosine_similarity(output1, output2)
 
-# fine tune a pre-trained Faster R-CNN
-# Freeze backbone layers
-def fine_tune(model):
-    for param in model.backbone.parameters():
-        param.requires_grad = False
 
-    # Only fine-tune the heads for classification and mask prediction
-    params_to_optimize = [p for p in model.parameters() if p.requires_grad]
-    return params_to_optimize
+def rcnn_feature_extraction(model, image):
+    with torch.no_grad():
+        features = model.backbone(image)
+    return features
+
+# Incomplete .. just thoughts
+def train_model(CNN, RCNN, frame):
+    with torch.no_grad():
+        # Pass frame through RCNN to get detections and features
+        features = rcnn_feature_extraction(rcnn, frame)
+    
+    # need to loop through all features....
+        # Pass through the Siamese network
+        feature1 = features[0].unsqueeze(0)  
+        feature2 = features[1].unsqueeze(0)  
+    
+        # Generate embeddings for comparison
+        embedding1 = CNN.forward_one(feature1)
+        embedding2 = CNN.forward_one(feature2)
+    
+        # Compute similarity between embeddings for tracking association
+        similarity_score = CNN.similarity(embedding1, embedding2)
+        print("Similarity Score:", similarity_score.item())
 
 
 if __name__ == '__main__':
-    pass
+    rcnn = fasterrcnn_resnet50_fpn(pretrained=False)
+    rcnn.load_state_dict(torch.load("rcnn_weights.pth"))  # Load your trained weights
+    rcnn.eval()  # Set RCNN to evaluation mode
+
+
     # do stuff here
 
 
 '''
 STEPS:
-1) load data
-2) augment data
-3) fine tune pretrained model (rcnn) on MOTS?
+1) load data -- yes?
+2) augment data -- yes?
+3) fine tune pretrained model (rcnn) on MOTS? -- next step
 4) pass info from rcnn and train siamese network 
 5) create tracking pipeline
 '''
