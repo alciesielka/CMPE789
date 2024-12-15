e54bef8380e1955bdc0ebe5a9bbcba6269a814d1
tiannaseitz <tjs1980@rit.edu>
2024-12-15 17:14:41 -0500

    having trouble with navigation data

--- project_final/test.py
+++ project_final/test.py
@@ -6,12 +6,13 @@ client.set_timeout(10.0)
 # List all available maps
 maps = client.get_available_maps()
 
-world = client.load_world("Town05_Opt", carla.MapLayer.Buildings | carla.MapLayer.ParkedVehicles)
+#world = client.load_world("Town03_Opt", carla.MapLayer.Buildings | carla.MapLayer.ParkedVehicles)
+world = client.load_world("Town02")
+
+print(f"Current world map: {world.get_map().name}")
 
 nav_point = world.get_random_location_from_navigation() 
 if not nav_point:
         print("Navigation data unavailable on this map.")
 
 print("Navigation point:", nav_point)
-
-print(f"Current world map: {world.get_map().name}")
